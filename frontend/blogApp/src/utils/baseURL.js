d3fc821d93c391ee81649a20cfc4a25f1b25290a
mazen ahmed <mahmedabdelgowad@gmail.com>
2024-03-27 02:03:26 +0200

    update url

--- frontend/blogApp/src/utils/baseURL.js
+++ frontend/blogApp/src/utils/baseURL.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "blog-app-api-seven.vercel.app",
+  baseURL: "https://blog-app-api-seven.vercel.app/",
 });
 
-export const BASE_URL = "blog-app-api-seven.vercel.app";
+export const BASE_URL = "https://blog-app-api-seven.vercel.app/";
 
 export default request;
